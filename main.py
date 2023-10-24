191a3333453c8591a13382e58972e15243b64b26
Deepak KN <55972417+kndeepak@users.noreply.github.com>
2023-10-24 17:15:01 -0500

    Update main.py with Fast API
    FAST API integration

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-import timeit
-import argparse
+from fastapi import FastAPI, HTTPException, Query
 from llm.wrapper import setup_rag_pipeline
-import re
+from typing import Dict
+import timeit
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input',
-                        type=str,
-                        default='What is the invoice number value?',
-                        help='Enter the query to pass into the LLM')
-    args = parser.parse_args()
+# Initialize the RAG pipeline once, rather than on every request.
+rag_pipeline = setup_rag_pipeline()
 
+@app.get("/get_answer/", response_model=Dict[str, str])
+async def get_answer(query: str = Query(default="What is the invoice number value?", description="Enter the query to pass into the LLM")):
     start = timeit.default_timer()
-    rag_pipeline = setup_rag_pipeline()
-
-    json_response = rag_pipeline.run(query=args.input, params={"Retriever": {"top_k": 5}})
-
+    
+    json_response = rag_pipeline.run(query=query, params={"Retriever": {"top_k": 5}})
+    
     answers = json_response['answers']
     answer = 'No answer found'
     for ans in answers:
         answer = ans.answer
         break
-
+    
     end = timeit.default_timer()
-
-    print(f'\nAnswer: {answer}')
-    print('='*50)
-
-    print(f"Time to retrieve answer: {end - start}")
+    time_taken = end - start
+    
+    return {
+        "answer": answer,
+        "time_taken": str(time_taken) + " seconds"
+    }
